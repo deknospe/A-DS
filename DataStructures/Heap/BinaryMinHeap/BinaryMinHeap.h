9c4176be912a799bff000426ad31081b474a2bd9
deknospe <deknospe@gmail.com>
2025-08-20 16:36:34 +0300

    done BinaryHeap

---
+++ DataStructures/Heap/BinaryMinHeap/BinaryMinHeap.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <vector>
+#include <stdexcept>
+
+
+template <typename T>
+class BinaryMinHeap {
+public:
+    BinaryMinHeap() noexcept = default;
+
+    explicit BinaryMinHeap(const std::vector<T> &arr) noexcept
+        : arr(arr)
+    {
+        for (size_t i = this->arr.size() / 2; i > 0; --i) {
+            this->siftDown(i);
+        }
+        if (!this->arr.empty()) [[likely]] {
+            this->siftDown(0);
+        }
+    }
+
+    T getMin() const noexcept {
+        return this->arr.front();
+    }
+
+    void removeMin() {
+        if (this->isEmpty()) [[unlikely]] {
+            throw std::out_of_range("Heap is empty");
+        }
+        this->arr.front() = std::move(this->arr.back());
+        this->arr.pop_back();
+        this->siftDown(0);
+    }
+
+    void insert(const T &elem) {
+        this->arr.push_back(elem);
+        this->siftUp(this->arr.size() - 1);
+    }
+
+    T extractMin()
+    {
+        const T minElem = std::move(this->arr.front());
+        this->arr.front() = std::move(this->arr.back());
+        this->arr.pop_back();
+        this->siftDown(0);
+        return minElem;
+    }
+
+    bool isEmpty() const noexcept {
+        return this->arr.empty();
+    }
+
+private:
+    std::vector<T> arr;
+
+    void siftUp(size_t index) noexcept {
+        while (index > 0 && this->arr[index] < this->arr[(index - 1) / 2]) {
+            std::swap(this->arr[index], this->arr[(index - 1) / 2]);
+            index = (index - 1) / 2;
+        }
+    }
+
+    void siftDown(size_t index) noexcept {
+        while (2 * index + 1 < this->arr.size()) {
+            size_t j = 2 * index + 1;
+            if (j + 1 < this->arr.size() && this->arr[j + 1] < this->arr[j]) {
+                ++j;
+            }
+            if (this->arr[index] < this->arr[j]) {
+                break;
+            }
+            std::swap(this->arr[index], this->arr[j]);
+            index = j;
+        }
+    }
+};
